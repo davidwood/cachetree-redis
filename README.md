a072c90d772b1ceb10eb820c740bebcf5b739520
David Wood <bitprobe@gmail.com>
2013-01-28 21:23:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# cachetree-redis [![Build Status](https://secure.travis-ci.org/davidwood/cachetree-redis.png)](http://travis-ci.org/davidwood/cachetree-redis)
+
+A Redis storage backend for [Cachetree](https://github.com/davidwood/cachetree).
+
+## Installation
+
+    npm install cachetree-redis
+
+## Usage
+
+To create a Cachtree instance using a Redis storage backend:
+
+```
+var cachetreeRedis = require('cachetree-redis'),
+    cachetree = require('cachetree),
+    store = cachetreeRedis(),
+    cache = cachetree(store);
+```
+
+The `cachetree-redis` module exports a single constructor function that accepts an optional options object and returns a Redis storage backend instance.
+
+`cachetreeRedis(options)`
+
+* `options`: An object containing the following configuration options:
+    * `client`: Redis client instance. If provided, `port`, `host`, `options`, `pw` and `db` are ignored
+    * `port`: Redis server port
+    * `host`: Redis server host
+    * `options`: [Redis client](https://github.com/mranney/node_redis) options passed to `redis.createClient`
+    * `pw`: Password for authenticating with Redis
+    * `pw`: Alias for `pw`, if you're not a fan of the whole brevity thing
+    * `db`: Number of the database to select
+    * `database`: Alias for `db`
+    * `delimiter`: Redis key delimiter, defaults to `:`
+
+The returned store exposes the underlying Redis client through a property named `client`.
+
+## Running Tests
+
+`cachetree-redis` tests require [Mocha](http://visionmedia.github.com/mocha/) and can be run with either `npm test` or `make test`.  You can specify Mocha options, such as the reporter, by adding a [mocha.opts](http://visionmedia.github.com/mocha/#mocha.opts) file, which is ignored by git, to the `test` directory.
+
+By default, the test suite uses [fakeredis](https://npmjs.org/package/fakeredis) to mock a Redis server.  To run the tests against an actual Redis server, add a JSON file named `options.json` to the `test` directory. This file should contain the options to be passed to the `cachetree-redis` constructor function.  Like the `mocha.opts` file, `options.json` is ignored by git.
---
+++ test/redisstore.js
@@ -0,0 +1,486 @@
+/*global describe: true, it:true, beforeEach: true, afterEach: true, before: true, after: true */
+var assert      = require('assert'),
+    fakeredis   = require('fakeredis'),
+    client      = fakeredis.createClient(),
+    RedisStore  = require('../');
+
+describe('RedisStore', function() {
+
+  var store;
+  before(function() {
+    var options;
+    try {
+      options = require('./options.json');
+    } catch (e) {
+    }
+    store = new RedisStore(options || { client: client });
+  });
+  
+  function clear(store, cb) {
+    store.client.keys('*', function(err, keys) {
+      if (Array.isArray(keys)) {
+        store.client.del(keys, function() {
+          cb();
+        });
+      } else {
+        cb();
+      }
+    });
+  }
+
+  var data = {
+    icao: {
+      alpha: 'dot dash',
+      bravo: 'dash dot dot dot',
+      charlie: 'dash dot dash dot',
+      delta: 'dash dot dot'
+    }
+  },
+  data2 = {
+    icao: {
+      alpha: 'dot dash',
+      bravo: 'dash dot dot dot'
+    },
+    'icao:more': {
+      charlie: 'dash dot dash dot',
+      delta: 'dash dot dot'
+    },
+    itu: {
+      echo: 'dot',
+      foxtrot: 'dot dot dash dot'
+    },
+    'alpha:icao': {
+      golf: 'dash dash dot',
+      hotel: 'dot dot dot dot'
+    }
+  };
+
+  it('should accept a custom delimiter', function() {
+    var inst = new RedisStore({ client: store.client, delimiter: '-' });
+    assert.equal(inst.delimiter, '-');
+  });
+
+  describe('.get(key, field, cb)', function() {
+    
+    before(function(done) {
+      store.client.hmset('icao', data.icao, function() {
+        done();
+      });
+    });
+
+    after(function(done) {
+      clear(store, done);
+    });
+
+    it('should return self for chaining', function() {
+      assert.strictEqual(store.get(), store);
+    });
+
+    it('should return an error if the key is not defined', function(done) {
+      store.get(function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Invalid key');
+        done();
+      });
+    });
+
+    it('should return a value if a single field is specified', function(done) {
+      store.get('icao', 'charlie', function(err, value) {
+        assert.ok(!err);
+        assert.equal(value, 'dash dot dash dot');
+        done();
+      });
+    });
+
+    it('should accept an array for the key', function(done) {
+      store.get(['icao'], 'charlie', function(err, value) {
+        assert.ok(!err);
+        assert.equal(value, 'dash dot dash dot');
+        done();
+      });
+    });
+
+    it('should return null if a single field is specified but does not exist', function(done) {
+      store.get('icao', 'echo', function(err, value) {
+        assert.ok(!err);
+        assert.equal(value, null);
+        done();
+      });
+    });
+
+    it('should return an object if multiple fields are specified', function(done) {
+      store.get('icao', 'charlie', 'alpha', function(err, values) {
+        assert.ok(!err);
+        assert.deepEqual(values, { charlie: 'dash dot dash dot', alpha: 'dot dash' });
+        done();
+      });
+    });
+
+    it('should return an object with values of null if multiple fields are specified but do not exist', function(done) {
+      store.get('icao', 'echo', 'foxtrot', function(err, values) {
+        assert.ok(!err);
+        assert.strictEqual(values, Object(values));
+        assert.deepEqual(Object.keys(values), ['echo', 'foxtrot']);
+        assert.deepEqual(values, { echo: null, foxtrot: null });
+        done();
+      });
+    });
+
+    it('should return an object with all fields if no fields are specified', function(done) {
+      store.get('icao', function(err, values) {
+        assert.ok(!err);
+        assert.notStrictEqual(values, data.icao);
+        assert.deepEqual(values, data.icao);
+        done();
+      });
+    });
+
+  });
+
+  describe('.set(key, field, value, cb)', function() {
+
+    beforeEach(function(done) {
+      clear(store, done);
+    });
+
+    after(function(done) {
+      clear(store, done);
+    });
+
+    it('should return self for chaining', function() {
+      assert.strictEqual(store.set(), store);
+    });
+
+    it('should return an error if the key is not defined', function(done) {
+      store.set(function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Invalid key');
+        done();
+      });
+    });
+
+    it('should accept single field and value arguments', function(done) {
+      store.set('icao', 'alpha', 'dot dash', function(err) {
+        assert.ok(!err);
+        store.client.hgetall('icao', function(err, data) {
+          assert.deepEqual(data, { alpha: 'dot dash' });
+          done();
+        });
+      });
+    });
+
+    it('should accept an array for the key', function(done) {
+      store.set(['icao'], 'alpha', 'dot dash', function(err) {
+        store.client.hgetall('icao', function(err, data) {
+          assert.deepEqual(data, { alpha: 'dot dash' });
+          done();
+        });
+      });
+    });
+
+    it('should accept multiple field and value arguments', function(done) {
+      store.set('icao', 'alpha', 'dot dash', 'bravo', 'dash dot dot dot', 'charlie', 'dash dot dash dot', 'delta', function(err) {
+        assert.ok(!err);
+        store.client.hgetall('icao', function(err, data) {
+          assert.deepEqual(data, { alpha: 'dot dash', bravo: 'dash dot dot dot', charlie: 'dash dot dash dot' });
+          done();
+        });
+      });
+    });
+
+    it('should accept an array of field and value arguments', function(done) {
+      store.set('icao', ['alpha', 'dot dash', 'bravo', 'dash dot dot dot', 'charlie', 'dash dot dash dot', 'delta'], function(err) {
+        assert.ok(!err);
+        store.client.hgetall('icao', function(err, data) {
+          assert.deepEqual(data, { alpha: 'dot dash', bravo: 'dash dot dot dot', charlie: 'dash dot dash dot' });
+          done();
+        });
+      });
+    });
+
+    it('should accept an object of field and values', function(done) {
+      store.set('icao', { alpha: 'dot dash', bravo: 'dash dot dot dot', charlie: 'dash dot dash dot' }, function(err) {
+        assert.ok(!err);
+        store.client.hgetall('icao', function(err, data) {
+          assert.deepEqual(data, { alpha: 'dot dash', bravo: 'dash dot dot dot', charlie: 'dash dot dash dot' });
+          done();
+        });
+      });
+    });
+
+  });
+
+  describe('.keys(pattern, cb)', function() {
+
+    before(function(done) {
+      var keys = Object.keys(data2),
+          active = keys.length;
+      if (active === 0) return done();
+      keys.forEach(function(key) {
+        store.client.hmset(key, data2[key], function() {
+          if (--active === 0) done();
+        });
+      });
+    });
+
+    after(function(done) {
+      clear(store, done);
+    });
+
+    it('should return self for chaining', function() {
+      assert.strictEqual(store.keys(), store);
+    });
+
+    it('should return an error if the pattern is not defined', function(done) {
+      store.keys(function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Invalid pattern');
+        done();
+      });
+    });
+
+    it('should return an error if the pattern is not a string or regular expression', function(done) {
+      store.keys({}, function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Invalid pattern');
+        done();
+      });
+    });
+
+    it('should accept a string pattern', function(done) {
+      store.keys('ic*', function(err, keys) {
+        assert.ok(!err);
+        assert.ok(Array.isArray(keys));
+        assert.deepEqual(keys, ['icao', 'icao:more']);
+        done();
+      });
+    });
+
+    it('should return an empty array if pattern is not found', function(done) {
+      store.keys('phonetic', function(err, keys) {
+        assert.ok(!err);
+        assert.ok(Array.isArray(keys));
+        assert.equal(keys.length, 0);
+        done();
+      });
+    });
+
+  });
+
+  describe('.exists(key, field, cb)', function() {
+
+    before(function(done) {
+      store.client.hmset('icao', data.icao, function() {
+        done();
+      });
+    });
+
+    after(function(done) {
+      clear(store, done);
+    });
+
+    it('should return self for chaining', function() {
+      assert.strictEqual(store.exists(), store);
+    });
+
+    it('should return an error if the key is not defined', function(done) {
+      store.exists(function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Invalid key');
+        done();
+      });
+    });
+
+    it('should return an error if the field is not defined', function(done) {
+      store.exists('icao', function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Invalid field');
+        done();
+      });
+    });
+
+    it('should accept an array for the key', function(done) {
+      store.exists(['icao'], 'bravo', function(err, exists) {
+        assert.ok(!err);
+        assert.equal(exists, true);
+        done();
+      });
+    });
+
+    it('should return true if the field exists', function(done) {
+      store.exists('icao', 'bravo', function(err, exists) {
+        assert.ok(!err);
+        assert.equal(exists, true);
+        done();
+      });
+    });
+
+    it('should return false if the field exists', function(done) {
+      store.exists('icao', 'foxtrot', function(err, exists) {
+        assert.ok(!err);
+        assert.equal(exists, false);
+        done();
+      });
+    });
+
+  });
+
+  describe('.del(key, field, cb)', function() {
+
+    before(function(done) {
+      store.client.hmset('icao', data.icao, function() {
+        done();
+      });
+    });
+
+    after(function(done) {
+      clear(store, done);
+    });
+
+    it('should return self for chaining', function() {
+      assert.strictEqual(store.del(), store);
+    });
+
+    it('should return an error if the key is not defined', function(done) {
+      store.del(function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Invalid key');
+        done();
+      });
+    });
+
+    it('should accept a single field', function(done) {
+      store.del('icao', 'alpha', function(err) {
+        assert.ok(!err);
+        store.client.hgetall('icao', function(err, data) {
+          assert.deepEqual(data, { bravo: 'dash dot dot dot', charlie: 'dash dot dash dot', delta: 'dash dot dot' });
+          done();
+        });
+      });
+    });
+
+    it('should accept an array for the key', function(done) {
+      store.del(['icao'], 'alpha', function(err) {
+        assert.ok(!err);
+        store.client.hgetall('icao', function(err, data) {
+          assert.deepEqual(data, { bravo: 'dash dot dot dot', charlie: 'dash dot dash dot', delta: 'dash dot dot' });
+          done();
+        });
+      });
+    });
+
+    it('should accept multiple fields', function(done) {
+      store.del('icao', 'alpha', 'charlie', 'echo', function(err) {
+        assert.ok(!err);
+        store.client.hgetall('icao', function(err, data) {
+          assert.deepEqual(data, { bravo: 'dash dot dot dot', delta: 'dash dot dot' });
+          done();
+        });
+      });
+    });
+
+    it('should accept an array of fields', function(done) {
+      store.del('icao', ['alpha', 'charlie', 'alpha'], function(err) {
+        assert.ok(!err);
+        store.client.hgetall('icao', function(err, data) {
+          assert.deepEqual(data, { bravo: 'dash dot dot dot', delta: 'dash dot dot' });
+          done();
+        });
+      });
+    });
+
+  });
+
+  describe('.flush(key, cb)', function() {
+
+    beforeEach(function(done) {
+      var keys = Object.keys(data2),
+          active = keys.length;
+      if (active === 0) return done();
+      keys.forEach(function(key) {
+        store.client.hmset(key, data2[key], function() {
+          if (--active === 0) done();
+        });
+      });
+    });
+
+    afterEach(function(done) {
+      clear(store, done);
+    });
+
+    it('should return self for chaining', function() {
+      assert.strictEqual(store.flush(), store);
+    });
+
+    it('should return an error if the key is not defined', function(done) {
+      store.flush(function(err) {
+        assert.ok(err instanceof Error);
+        assert.equal(err.message, 'Invalid key');
+        done();
+      });
+    });
+
+    it('should accept a single field', function(done) {
+      store.flush('icao', 'alpha', function(err) {
+        assert.ok(!err);
+        store.client.exists('icao', function(err, exists) {
+          assert.equal(exists, 0);
+          done();
+        });
+      });
+    });
+
+    it('should accept an array for they key', function(done) {
+      store.flush(['icao'], 'alpha', function(err) {
+        assert.ok(!err);
+        store.client.exists('icao', function(err, exists) {
+          assert.equal(exists, 0);
+          done();
+        });
+      });
+    });
+
+    it('should accept multiple fields', function(done) {
+      store.flush('icao', 'icao:more', 'echo', function(err) {
+        assert.ok(!err);
+        store.client.exists('icao', function(err, exists) {
+          assert.equal(exists, 0);
+          store.client.exists('icao:more', function(err, exists) {
+            assert.equal(exists, 0);
+            done();
+          });
+        });
+      });
+    });
+
+    it('should accept an array of fields', function(done) {
+      store.flush(['icao', 'icao:more', 'icao'], function(err) {
+        assert.ok(!err);
+        store.client.exists('icao', function(err, exists) {
+          assert.equal(exists, 0);
+          store.client.exists('icao:more', function(err, exists) {
+            assert.equal(exists, 0);
+            done();
+          });
+        });
+      });
+    });
+
+  });
+
+  describe('.cacheKey(key)', function() {
+    
+    it('should return an array concatenated with delimiter', function() {
+      assert.equal(store.cacheKey(['alpha', 'bravo']), 'alpha:bravo');
+      var inst = new RedisStore({ client: store.client, delimiter: '-' });
+      assert.equal(inst.cacheKey(['alpha', 'bravo']), 'alpha-bravo');
+    });
+
+    it('should return the value if a string or number', function() {
+      assert.equal(store.cacheKey(['alpha', 'bravo']), 'alpha:bravo');
+      assert.equal(store.cacheKey(1234), 1234);
+    });
+
+  });
+
+});
